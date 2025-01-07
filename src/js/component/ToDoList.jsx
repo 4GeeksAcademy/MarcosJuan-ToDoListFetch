d4ea7d3041d3c301a577e894b5a6dd1fcfd53cdf
Marcos Juan Martos Prados <martospradosmarcos@gmail.com>
2025-01-07 19:58:50 +0000

    Project done

--- src/js/component/ToDoList.jsx
+++ src/js/component/ToDoList.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 
 export const ToDoList = () => {
-    const [ todos, setTodos ] = useState([]);
-    const [ newTask, setNewTask ] = useState("");
-    const [ editTask, setEditTask ] = useState(null);
-    const [ editMode, setEditMode ] = useState(false);
-    const host = 'https://playground.4geeks.com/todo'
-    const user = 'MarcosJuan'
+    const [todos, setTodos] = useState([]);
+    const [newTask, setNewTask] = useState("");
+    const [editTask, setEditTask] = useState(null);
+    const [editMode, setEditMode] = useState(false);
+    const [isDone, setIsDone] = useState(false);
+    const host = "https://playground.4geeks.com/todo";
+    const user = "MarcosJuan";
 
     useEffect(() => {
         showTask();
@@ -15,24 +16,25 @@ export const ToDoList = () => {
     // Obtener las tareas
     const showTask = async () => {
         const uri = `${host}/users/${user}`;
-        const options = { 
-            method: 'GET'
+        const options = {
+            method: "GET"
         };
 
         const response = await fetch(uri, options);
         if (!response.ok) {
-            console.error('Error: ', response.status, response.statusText);
+            console.error("Error: ", response.status, response.statusText);
             return;
         }
 
         const data = await response.json();
-        console.log(data)
-        setTodos(data.todos); 
+        setTodos(data.todos);
     };
 
     // Crear nueva tarea
     const addTask = async (event) => {
         event.preventDefault();
+        if (!newTask.trim()) return;
+
         const dataToSend = {
             label: newTask,
             is_done: false,
@@ -40,41 +42,42 @@ export const ToDoList = () => {
 
         const uri = `${host}/todos/${user}`;
         const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(dataToSend),
         };
 
         const response = await fetch(uri, options);
         if (!response.ok) {
-            console.error('Error: ', response.status, response.statusText);
+            console.error("Error: ", response.status, response.statusText);
             return;
         }
 
-        // Traer devuelta todas las tareas ejecutando la funcion que hace el get
-        setNewTask('');
+        setNewTask("");
         showTask();
     };
 
     // Borrar tarea
-    const deleteTask = async (taskId) => {
-        const uri = `${host}/todos/${user}`;
-        const options = { 
-            method: "DELETE" 
+    const handleDeleteTask = async (taskId) => {
+        const uri = `${host}/todos/${taskId}`;
+        const options = {
+            method: "DELETE"
         };
 
         const response = await fetch(uri, options);
         if (!response.ok) {
-            console.error("Error al eliminar tarea:", response.status, response.statusText);
+            console.error(
+                "Error al eliminar tarea:",
+                response.status,
+                response.statusText
+            );
             return;
         }
-
+        showTask()
         setTodos((prevTodos) => prevTodos.filter((task) => task.id !== taskId));
     };
 
-    // Editar tarea
+    // Preparar tarea para edición
     const handleEdit = (task) => {
         setEditTask(task);
         setEditMode(true);
@@ -85,40 +88,91 @@ export const ToDoList = () => {
     const saveTask = async (event) => {
         event.preventDefault();
         if (!editTask) return;
-        const updatedTask = {
-            ...editTask,
-            label: newTask,
-        };
 
-        const uri = `${host}/todos/${user}`;
+        const dataToSend = {
+            label: newTask, 
+            is_done: isDone
+        }
+
+        const uri = `${host}/todos/${editTask.id}`;
         const options = {
             method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(updatedTask),
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(dataToSend),
         };
 
         const response = await fetch(uri, options);
-
         if (!response.ok) {
-            console.log("Error al editar la tarea:", response.status, response.statusText);
+            console.error(
+                "Error al editar la tarea:",
+                response.status,
+                response.statusText
+            );
             return;
         }
 
-        setTodos((prevTodos) =>
-            prevTodos.map((task) => (task.id === editTask.id ? updatedTask : task))
-        );
+        showTask()
 
         setEditMode(false);
         setEditTask(null);
         setNewTask("");
     };
 
-
     return (
-        <div>
-            <h1>ToDoList</h1>
+        <div className="container">
+            <h1 className="text-center mt-4 text-success">ToDo</h1>
+            <div className="mb-3">
+                <form onSubmit={editMode ? saveTask : addTask}>
+                    <input
+                        className="form-control"
+                        placeholder="Add task"
+                        type="text"
+                        value={newTask}
+                        onChange={(e) => setNewTask(e.target.value)}
+                    />
+                    {editMode ?
+                    <div>
+                        <input
+                            className="form-check-input mb-2"
+                            type="checkbox"
+                            checked={isDone}
+                            onChange={(e) => setIsDone(e.target.value)}
+                        />
+                        <label className="form-check-label ms-2">Completed Task</label>
+                    </div>
+                        : ''}
+                    <button type="submit" className="btn btn-primary mt-3">
+                        {editMode ? "Save changes" : "Add task"}
+                    </button>
+                </form>
+            </div>
+            <h2 className="text-primary text-center">My Todos</h2>
+            <div className="list">
+                <ul className="list-group">
+                    {todos.map((item) => (
+                        <li
+                            key={item.id}
+                            className="list-group-item d-flex justify-content-between align-items-center"
+                        >
+                            {item.is_done ? "✔️" : "❌"} {item.label}
+                            <div>
+                            <span onClick={() => handleEdit(item)}>
+                                <i className="fas fa-pencil-alt text-success me-3"></i>
+                            </span>
+                            <span onClick={() => handleDeleteTask(item.id)}>
+                                <i className="far fa-trash-alt text-danger"></i>
+                            </span>
+                            </div>
+                        </li>
+                    ))}
+                    <span className="list-group-item bg-light text-end fw-lighter">
+                        {todos.length === 0
+                            ? "No tasks, do you want to add?"
+                            : `${todos.length} tasks`}
+                    </span>
+                </ul>
+            </div>
         </div>
-    )
-}
+    );
+};
+
